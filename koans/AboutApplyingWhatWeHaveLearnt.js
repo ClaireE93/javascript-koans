9cd7e8445387afaeda7c19b307c7137d6d4addc2
Claire Johnson <c.eliseee@gmail.com>
2017-07-25 09:24:01 -0700

    Passed all tests, except for extra credit.

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -63,9 +63,16 @@ describe("About Applying What We Have Learnt", function() {
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+    var arr = _.range(1, 1000);
+    var sum = _.chain(arr)
+                .reduce((acc, cur) => {
+                  if(cur % 5 === 0 || cur % 3 ===0) {
+                    return acc + cur;
+                  }
+                  return acc;
+                }, 0).value();/* try chaining range() and reduce() */
 
-    expect(233168).toBe(FILL_ME_IN);
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
@@ -78,15 +85,24 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   it("should count the ingredient occurrence (functional)", function () {
-    var ingredientCount = { "{ingredient name}": 0 };
+    // var ingredientCount= {};
+    var ingredientCount = _.chain(products)
+                          .map(cur => {
+                            return cur.ingredients;
+                          }).flatten()
+                          .reduce((acc, cur) => {
+                            acc[cur] = (acc[cur] || 0) + 1;
+                            return acc;
+                          },{}).value();
 
     /* chain() together map(), flatten() and reduce() */
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   /*********************************************************************************/
